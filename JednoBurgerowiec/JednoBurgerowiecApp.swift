2223de71544519f5b338b178247064f42d54a192
Jan Klausa <jan@klausa.pl>
2020-07-03 13:38:13 +0200

    Initial Commit

---
+++ JednoBurgerowiec/JednoBurgerowiecApp.swift
@@ -0,0 +1,17 @@
+//
+//  JednoBurgerowiecApp.swift
+//  JednoBurgerowiec
+//
+//  Created by Jan Klausa on 03.07.20.
+//
+
+import SwiftUI
+
+@main
+struct JednoBurgerowiecApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
